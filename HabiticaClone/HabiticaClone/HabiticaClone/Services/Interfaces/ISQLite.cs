b99ad3a1dc61a409379c2500c3d0b95f88b901dd
Joel Pickin <joel.pickin@Viewpoint.com>
2019-08-16 09:23:38 +0100

    * Started implementing SqlLite

---
+++ HabiticaClone/HabiticaClone/HabiticaClone/Services/Interfaces/ISQLite.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabiticaClone.Services.Interfaces
+{
+    public interface ISQLite
+    {
+        SQLiteConnection GetConnection();
+    }
+}
